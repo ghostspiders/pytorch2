41ff944ab1168fb5ac927dd213730b27a54fd256
gaoyufeng <15834128411@126.com>
2025-05-30 03:17:59 +0800

    first

--- c10/Device.cpp
+++ c10/Device.cpp
@@ -36,29 +36,7 @@ DeviceType parse_type(const std::string& device_string) {
 }
 } // namespace
 
-// `std::regex` is still in a very incomplete state in GCC 4.8.x,
-// so we have to do our own parsing, like peasants.
-// https://stackoverflow.com/questions/12530406/is-gcc-4-8-or-earlier-buggy-about-regular-expressions
-//
-// Replace with the following code once we shed our GCC skin:
-//
-// static const std::regex regex(
-//     "(cuda|cpu)|(cuda|cpu):([0-9]+)|([0-9]+)",
-//     std::regex_constants::basic);
-// std::smatch match;
-// const bool ok = std::regex_match(device_string, match, regex);
-// AT_CHECK(ok, "Invalid device string: '", device_string, "'");
-// if (match[1].matched) {
-//   type_ = parse_type_from_string(match[1].str());
-// } else {
-//   if (match[2].matched) {
-//     type_ = parse_type_from_string(match[1].str());
-//   } else {
-//     type_ = Type::CUDA;
-//   }
-//   AT_ASSERT(match[3].matched);
-//   index_ = std::stoi(match[3].str());
-// }
+
 Device::Device(const std::string& device_string) : Device(Type::CPU) {
   AT_CHECK(!device_string.empty(), "Device string must not be empty");
   int index = device_string.find(":");
--- c10/DeviceGuard.h
+++ c10/DeviceGuard.h
@@ -4,18 +4,7 @@
 
 namespace c10 {
 
-/// RAII guard that sets a certain default device in its constructor, and
-/// changes it back to the device that was originally active upon destruction.
-///
-/// The device is always reset to the one that was active at the time of
-/// construction of the guard. Even if you `set_device` after construction, the
-/// destructor will still reset the device to the one that was active at
-/// construction time.
-///
-/// This device guard does NOT have an uninitialized state; it is guaranteed
-/// to reset a device on exit.  If you are in a situation where you *might*
-/// want to setup a guard (i.e., are looking for the moral equivalent
-/// of optional<DeviceGuard>), see OptionalDeviceGuard.
+
 class DeviceGuard {
 public:
   /// No default constructor; see Note [Omitted default constructor from RAII]
@@ -36,13 +25,7 @@ public:
   DeviceGuard(DeviceGuard&& other) = delete;
   DeviceGuard& operator=(DeviceGuard&& other) = delete;
 
-  /// Sets the device to the given one.  The specified device must be consistent
-  /// with the device type originally specified during guard construction.
-  ///
-  /// TODO: The consistency check here is inconsistent with StreamGuard's
-  /// behavior with set_stream, where a stream on a different device than
-  /// the original one isn't an error; we just reset the stream and then
-  /// switch devices.
+
   void reset_device(at::Device device) {
     guard_.reset_device(device);
   }
@@ -73,49 +56,7 @@ private:
   impl::InlineDeviceGuard<impl::VirtualGuardImpl> guard_;
 };
 
-/**
- * A OptionalDeviceGuard is an RAII class that sets a device to some value on
- * initialization, and resets the device to its original value on destruction.
- * Morally, a OptionalDeviceGuard is equivalent to optional<DeviceGuard>, but
- * with extra constructors and methods as appropriate.
- *
- * Besides its obvious use (optionally applying a DeviceGuard), OptionalDeviceGuard
- * is often also used for the following idiom:
- *
- *    OptionalDeviceGuard g;
- *    for (const auto& t : tensors) {
- *      g.set_device(t.device());
- *      do_something_with(t);
- *    }
- *
- * This usage is marginally more efficient than constructing a DeviceGuard every
- * iteration of the for loop, as it avoids an unnecessary device reset.
- *
- * Unlike DeviceGuard, a OptionalDeviceGuard may be uninitialized.  This occurs
- * when you use the nullary constructor, or pass a nullopt to the constructor.
- * Uninitialized OptionalDeviceGuards do *nothing*; they do not know what the
- * original device was and they do not reset on destruction.  This is why
- * original_device() and current_device() return optional<Device> rather than
- * Device (as they do in DeviceGuard), and also is why we didn't just
- * provide OptionalDeviceGuard by default and hide DeviceGuard from users.
- *
- * The semantics of an OptionalDeviceGuard are exactly explained by thinking
- * of it as an optional<DeviceGuard>.  In particular, an initialized
- * OptionalDeviceGuard doesn't restore device to its value at construction; it
- * restores device to its value *at initialization*.  So if you have the
- * program:
- *
- *     setDevice(1);
- *     OptionalDeviceGuard g;
- *     setDevice(2);
- *     g.set_device(3);  // initializes!
- *
- * On destruction, g will reset device to 2, rather than 1.
- *
- * An uninitialized OptionalDeviceGuard is distinct from a (initialized)
- * DeviceGuard whose original_device_ and current_device_ match, since the
- * DeviceGuard will still reset the device to original_device_.
- */
+
 class OptionalDeviceGuard {
 public:
   /// Create an uninitialized guard.  Set the guard later using set_device.
@@ -168,17 +109,6 @@ private:
   impl::InlineOptionalDeviceGuard<impl::VirtualGuardImpl> guard_;
 };
 
-// Note [Whither the DeviceGuard boilerplate]
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Design note: in principle, we could avoid these wrappers using:
-//
-// using DeviceGuard = impl::InlineDeviceGuard<impl::VirtualGuardImpl>;
-// using OptionalDeviceGuard = impl::InlineOptionalDeviceGuard<impl::VirtualGuardImpl>;
-//
-// But the error messages are worse, and our users can't just look at the
-// header file to find out what's going on.  Furthermore, for specializations
-// like CUDAStreamGuard, it can be profitable to replace some interfaces with
-// refined types (e.g., return CUDAStream instead of Stream).  So, we eat
-// the boilerplate and write out the API explicitly.
+
 
 } // namespace c10
